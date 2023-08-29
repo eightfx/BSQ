1c1d8819e9f7d659a38be9355bfde44e018f2a1e
eight <eight@eightnoiMac.local>
2023-08-29 16:43:57 +0900

    [update]:

---
+++ utils.c
@@ -0,0 +1,49 @@
+
+#include <stddef.h>
+
+char	*ft_strncpy(char *dest, char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+int	ft_atoi(char *str)
+{
+	int	num;
+
+	num = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		num = num * 10 + (*str - '0');
+		str++;
+	}
+	return (num);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	char	ch;
+
+	ch = (char)c;
+	while (*s != '\0')
+	{
+		if (*s == ch)
+			return ((char *)s);
+		s++;
+	}
+	if (ch == '\0')
+		return ((char *)s);
+	return (NULL);
+}
